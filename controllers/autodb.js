1c826ff5f27e67587d07c655ffad3ab56aab9490
grynka <ihor.kozhemyakin@gmail.com>
2023-03-20 16:08:17 +0200

    add mariadb

--- controllers/autodb.js
+++ controllers/autodb.js
@@ -1,12 +1,12 @@
 const { HttpError, ctrlWrapper } = require("../helpers");
-//const mariadb = require("mariadb");
+const mariadb = require("mariadb");
 
-const pool = `mariadb.createPool({
+const pool = mariadb.createPool({
   host: process.env.SERVER,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-})`;
+});
 
 const manufactures = async (req, res) => {
   const connection = await pool.getConnection();
